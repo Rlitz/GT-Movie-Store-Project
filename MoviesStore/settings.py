f7a387198d819caf5e97c6177faf005646b54e23
Zhaoyyyyyyyy <rayzhao05@gmail.com>
2025-02-01 21:46:57 -0500

    made items and orders models and can be modified in admin

--- MoviesStore/settings.py
+++ MoviesStore/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
-    'moveies.app.MoviesConfig',
+    'movies.apps.MoviesConfig',
     'cart.apps.CartConfig'
 ]
 
