3e46e9ee48c978c749736f8a013cd4e85cc7d19a
Hoc <hocn7379@gmail.com>
2025-02-03 06:22:28 -0500

    Add navigation bar and logo

--- MoviesStore/settings.py
+++ MoviesStore/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
-    BASE_DIR / 'MoviesStore/static/',
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
