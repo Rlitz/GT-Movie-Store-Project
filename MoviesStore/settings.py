46f396340cb66d386871130e6797bfa9263f6c1e
Hoc <hocn7379@gmail.com>
2025-02-03 04:40:53 -0500

    Remove compiled Python files, and ignored files applied in git ignore, fix base template ref in home index.html

--- MoviesStore/settings.py
+++ MoviesStore/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
     'movies.apps.MoviesConfig',
-    'cart.apps.CartConfig'
+    'cart.apps.CartConfig',
     'accounts',
 ]
 
@@ -59,8 +59,7 @@ ROOT_URLCONF = 'MoviesStore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
