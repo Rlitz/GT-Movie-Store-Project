9608c6063fe1a4eb5c78ed12275c297844d31582
Cardin <cardinhschool@gmail.com>
2025-02-03 21:48:53 -0500

    merged with carts branch and fixed cart utility

--- MoviesStore/urls.py
+++ MoviesStore/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('cart/', include('cart.urls')),
     path('', include('home.urls')),
 ]
 
