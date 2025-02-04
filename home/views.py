1d250730332cf3e8787dea232186b32fd5d2e10b
Hoc <hocn7379@gmail.com>
2025-02-03 20:17:23 -0500

    push for cardin pull

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     template_data = {'title': 'Movies Store'}
-    return render(request, 'home/index.html', template_data)
+    return render(request, 'index.html', template_data)
 def about(request):
     template_data = {'title': 'About'}
-    return render(request, 'home/about.html', template_data)
+    return render(request, 'about.html', template_data)
