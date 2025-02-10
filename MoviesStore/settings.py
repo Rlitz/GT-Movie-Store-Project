131a4d1cc2a0ed486df4be67f0fb3b534cc663c7
rxjdasiwzl <2954235311@qq.com>
2025-02-10 18:44:51 -0500

    while not logged in, clicking the purchase button redirects the user to the log in page.

--- MoviesStore/settings.py
+++ MoviesStore/settings.py
@@ -18,7 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 LOGIN_URL = '/accounts/login'
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
