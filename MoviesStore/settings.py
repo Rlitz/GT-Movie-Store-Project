8c975ed8aad5f3e2946ef3a2ee23b43bc2f5446f
Hoc <hocn7379@gmail.com>
2025-02-11 13:03:27 -0500

    Resolve merge issues

--- MoviesStore/settings.py
+++ MoviesStore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-c7!-b_yfy%6@@yk7sd)xj=d7y_fp2bw&c5o_e8ksuy%iauz7(n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
